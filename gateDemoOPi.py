eb3f1b4da47fdf4df2b05e50ae6d9c5e1ff15fb8
Mikhail <kovalevski.m@yandex.ru>
2020-06-05 00:23:47 +0300

    Second

---
+++ gateDemoOPi.py
@@ -0,0 +1,22 @@
+from flask import Flask
+import OPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(21, GPIO.OUT)
+
+app = Flask('openGate')
+
+@app.route('/')
+def index():
+    return'u r now in gate_opener. To open gate send /open_gate'
+
+@app.route('/open_gate')
+def open_gate():
+    GPIO.output(21, GPIO.HIGH)
+    return 'opened'
+@app.route('/close_gate')
+def close_gate():
+    GPIO.output(21, GPIO.LOW)
+    return 'closed'
+
+app.run(debug=True, port=3000, host='192.168.0.100')
